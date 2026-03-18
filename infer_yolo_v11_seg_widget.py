ea90aa49ce796e0513cb85a11c04b8740d4db782
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-18 14:43:17 +0100

    chore: Qt 6

--- infer_yolo_v11_seg_widget.py
+++ infer_yolo_v11_seg_widget.py
@@ -1,10 +1,12 @@
+# PyQt GUI framework
+from PyQt6.QtWidgets import *
+
+from torch.cuda import is_available
+
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
-from infer_yolo_v11_seg.infer_yolo_v11_seg_process import InferYoloV11SegParam
 
-# PyQt GUI framework
-from PyQt5.QtWidgets import *
-from torch.cuda import is_available
+from infer_yolo_v11_seg.infer_yolo_v11_seg_process import InferYoloV11SegParam
 
 # --------------------
 # - Class which implements widget associated with the algorithm
@@ -51,7 +53,7 @@ class InferYoloV11SegWidget(core.CWorkflowTaskWidget):
         self.browse_weight_file = pyqtutils.BrowseFileWidget(
                                         path=self.parameters.model_weight_file,
                                         tooltip="Select file",
-                                        mode=QFileDialog.ExistingFile
+                                        mode=QFileDialog.FileMode.ExistingFile
         )
         row = self.grid_layout.rowCount()
         self.grid_layout.addWidget(self.label_hyp, row, 0)
